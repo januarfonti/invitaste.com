810ef8551a3cfc3400a07b50ae2089169918afa0
Januar Fonti <me@januarfonti.com>
2019-09-03 00:55:33 +0700

    add scroll to top function

--- src/components/scroll-to-top.js
+++ src/components/scroll-to-top.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons'
+import Scroll from 'react-scroll'
+
+var scroll = Scroll.animateScroll
 
 class Layout extends React.Component {
+  constructor (props) {
+    super(props)
+    this.scrollToTop = this.scrollToTop.bind(this)
+  }
+  scrollToTop () {
+    scroll.scrollToTop()
+  }
   render () {
     return (
-      <div className='scroll-to-top rounded bg-teal-900 fixed px-4 py-2'>
+      <div className='scroll-to-top rounded bg-teal-900 fixed px-4 py-2' onClick={this.scrollToTop}>
         <FontAwesomeIcon className='text-white' icon={faAngleDoubleUp} />
       </div>
     )
